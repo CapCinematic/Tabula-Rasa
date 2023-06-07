8f9ab6280052b51f66120b2901cef68e93b62f99
Isaiah Richardson <capcinematic@gmail.com>
2023-06-07 13:12:30 -0600

    feat/Homepage-component-complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import FetchData from './apiCalls';
+import Homepage from './Hompage';
+import './Homepage.css'
 
 function App() {
   return (
-    <div>
-      <h1>Philosophers</h1>
-      <FetchData />
-    </div>
+   <Homepage/>
   );
 }
 
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,48 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 const FetchData = () => {
-  const [philosophers, setPhilosopher] = [];
-  const [error, setError] = "";
-
-  const handleErrors = (response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response;
-  }
+  const [Objects, setObjects] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(`https://philosophyapi.pythonanywhere.com/api/philosophers/1/`)
-      .then(handleErrors)
+    fetch("https://api.quotable.io/quotes")
       .then((response) => response.json())
-      .then((data) => setPhilosopher(data))
-      .then(console.log(setPhilosopher, "data"))
+      .then((data) => setObjects(data))
       .catch((error) => setError(`Error: ${error}`));
-  }, [setPhilosopher]);
-
-  return (
-    <div>
+    }, []);
+    
+    return (
+      <div>
       {error ? (
         <p>{error}</p>
-      ) : (
-        <ul> {philosophers}
-          {/* {philosophers.map((philosopher) => (
-            <div key={philosopher.id}>
-              <h2>{philosopher.name}</h2>
-              <img src={philosopher.photo} alt={philosopher.name} />
-              <p>Born: {philosopher.born_date}</p>
-              <p>Died: {philosopher.death_date}</p>
-              <p>Nationality: {philosopher.nationality}</p>
-              <p>Era: {philosopher.era}</p>
-              <p>Schools: {philosopher.school.join(", ")}</p>
-              <p>Ideas: {philosopher.ideas.join(", ")}</p>
-              <p>Books: {philosopher.books.join(", ")}</p>
-            </div> 
-          ))} */}
-        </ul>
+        ) : (
+          <p>
+         Objects {Objects.count}
+        </p>
       )}
     </div>
   );
 };
+
 
 export default FetchData;
