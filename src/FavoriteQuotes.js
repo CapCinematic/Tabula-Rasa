2c3b46123e4951a114407698883df9738070a848
Isaiah Richardson <capcinematic@gmail.com>
2023-06-10 14:49:09 -0600

    formatted-code

--- src/FavoriteQuotes.js
+++ src/FavoriteQuotes.js
@@ -1,21 +1,17 @@
 import React from "react";
-import './FavoriteQuotes.css'
+import "./FavoriteQuotes.css";
 
-function FavoriteQuotes ({favorites}) {
+function FavoriteQuotes({ favorites }) {
   const selectedQuotes = favorites.map((favorite) => {
     return (
-      <div className='favorited-quotes' key={favorite._id}>
-          <p>Author:{favorite.author}</p>
-          <p>Quote:{favorite.content}</p>
+      <div className="favorited-quotes" key={favorite._id}>
+        <p>Author:{favorite.author}</p>
+        <p>Quote:{favorite.content}</p>
       </div>
-    )
+    );
   });
 
-  return(
-    <div>
-      {selectedQuotes}
-    </div>
-  )
+  return <div>{selectedQuotes}</div>;
 }
 
-export default FavoriteQuotes
+export default FavoriteQuotes;
--- src/Hompage.js
+++ src/Hompage.js
@@ -1,33 +1,33 @@
-import React , { Component } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Homepage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { quote: {} }
+    this.state = { quote: {} };
   }
 
-  componentDidMount (){
-    fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(data => this.setState({ quote: data }))
+  componentDidMount() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => this.setState({ quote: data }));
   }
 
-  render(){
-    const { quote } = this.state
+  render() {
+    const { quote } = this.state;
 
     return (
       <section className="home-page">
-      <h1>Welcome</h1>
-      <div className="quote-box">
-        {quote.content} - {quote.author}
-      </div>
-      <Link to='/search'>
-      <button className="enter-button">Enter</button>
-      </Link>
-    </section>
-    )
+        <h1>Welcome</h1>
+        <div className="quote-box">
+          {quote.content} - {quote.author}
+        </div>
+        <Link to="/search">
+          <button className="enter-button">Enter</button>
+        </Link>
+      </section>
+    );
   }
 }
 
-export default Homepage
+export default Homepage;
