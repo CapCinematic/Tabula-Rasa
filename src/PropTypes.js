5387304db2d635b82615d81f19b2c75f0e8baa4f
Isaiah Richardson <capcinematic@gmail.com>
2023-06-10 15:21:21 -0600

    Feature/AuthorPropTypes-component

---
+++ src/PropTypes.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+const AuthorPropTypes = {
+  authors: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      quoteCount: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+    })
+  ),
+    selectedAuthor: PropTypes.string,
+    quotes: PropTypes.arrayOf(
+      PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired,
+        length: PropTypes.number.isRequired,
+        tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+      })
+    ),
+}
