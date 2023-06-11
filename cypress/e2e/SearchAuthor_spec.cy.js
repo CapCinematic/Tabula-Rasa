10d53df17d6512f032b4d89a842ae71991c206dd
Isaiah Richardson <capcinematic@gmail.com>
2023-06-10 20:58:49 -0600

    refactor/continue-testing-searchauthor

--- cypress/e2e/SearchAuthor_spec.cy.js
+++ cypress/e2e/SearchAuthor_spec.cy.js
@@ -1,13 +1,24 @@
-describe('SearchAuthor', () => {
-  it('should display a list of authors', () => {
-    cy.visit('http://localhost:3000/search');
+// import { cy } from 'cypress'
 
-    cy.get('.authors-list li').should('have.length', 20)
+describe("SearchAuthor", () => {
+  it("should display a list of authors when data is fetched", () => {
+    cy.visit("/search");
+    cy.get("li").should("have.length.greaterThan", 0);
   });
 
-  it('displays quotes by selected author', () => {
-    cy.visit('http://localhost:3000/search')
+  it("display quotes by the selected author when an author is clicked", () => {
+    cy.visit("/search");
+    cy.get("li").first().click();
+    cy.get("h3").should("have.text", " Quotes by 14th Dalai Lama");
+    cy.get("ul").should("be.visible");
+  });
 
-    cy.get('.authors-list li').first().click();
-  })
-})
+  it("adds a quote to the favorites list when the 'Favorite' button is clicked", () => {
+    cy.visit("/search");
+    cy.get("li").eq(1).click();
+    cy.get("button").first().click();
+    cy.get("aside").should("be.visible");
+  });
+
+});
+
